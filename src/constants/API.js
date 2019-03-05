f81f63bd337a9e9bf3d2331b371a887cdd566b36
riccardo <riccardo.graziosi97@gmail.com>
2019-03-05 21:08:23 +0000

    Change versionCode to 8

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,3 +1,2 @@
-// export const API_URL = __DEV__ ? 'http://100.68.144.216:3000' : 'https://spaziorap.herokuapp.com';
-export const API_URL = 'https://spaziorap.herokuapp.com';
+export const API_URL = __DEV__ ? 'http://100.65.12.166:3000' : 'https://spaziorap.herokuapp.com';
 export const WEBSITE_URL = 'http://spaziorap.altervista.org';
